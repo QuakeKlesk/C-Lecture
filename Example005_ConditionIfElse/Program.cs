b7e5d5e903b5c62255344e670fa21f0282fb4f39
KleskQuake <Konovalovs777@mail.ru>
2022-08-08 20:38:13 +0700

    Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, этоже МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
