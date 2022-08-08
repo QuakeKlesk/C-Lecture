8355999bdd8cbb814c0a42cac9b8c325adfe4aaa
KleskQuake <Konovalovs777@mail.ru>
2022-08-08 21:05:08 +0700

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 5;
+int c = 10;
+int d = 7;
+int e = 3;
+
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
